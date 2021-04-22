8e4839238d6156a2ac8f3bad1ffd8117c943a651
pranali <kirolkar.pranali6@gmail.com>
2021-04-22 22:50:08 +0530

    customcomponent
    - data.js

--- textdemo/src/components/App/data.js
+++ textdemo/src/components/App/data.js
@@ -10,18 +10,12 @@ export default [
     scientificName: 'Gorilla beringei',
     size: 205,
     diet: ['plants', 'insects'],
-    additional: {
-      notes: 'This is the eastern gorilla. There is also a western gorilla that is a different species.'
-    }
   },
   {
     name: 'Zebra',
     scientificName: 'Equus quagga',
     size: 322,
     diet: ['plants'],
-    additional: {
-      notes: 'There are three different species of zebra.',
-      link: 'https://en.wikipedia.org/wiki/Zebra'
-    }
+    
   }
 ]
